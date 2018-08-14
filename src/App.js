3c06876f9f4dacacbbb8630d463a8fd7c7a6b4d9
Daniel Childs <dc.1994@outlook.com>
2018-08-14 21:01:08 +0100

    creates NavBar.js, adds react-router-dom to package.json

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React, { Component } from 'react';
-import HomePage from './containers/homepage.js'
+import React, { Component } from 'react'
+import HomePage from './containers/HomePage.js'
+import NavBar from './containers/NavBar.js'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
-      <HomePage/>
+      <Router>
+        <React.Fragment>
+          <NavBar />
+          <Route exact path = '/' Component={HomePage} />
+          {/* uncomment as you add the pages */}
+          {/* <Route path = '/about' Component={About} />
+          <Route path = '/projects' Component={Projects} />
+          <Route path = '/contact' Component={Contact} /> */}
+        </React.Fragment>
+      </Router>
     );
   }
 }
--- src/containers/homepage.js
+++ src/containers/homepage.js
@@ -3,7 +3,9 @@ import React, {Component} from 'react';
 class HomePage extends Component {
     render(){
         return(
-            <p>Hello</p>
+            <div className="intro">
+            <p>Let me introduce myself. I am Daniel Childs a Software Engineer based in Glasgow.</p>
+            </div>
         )
     }
 }
